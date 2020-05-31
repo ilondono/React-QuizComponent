1782bad00c0a14b980ab4395271c31778251ba34
ivandlondono <ivandlondono@gmail.com>
2020-05-30 20:41:42 -0500

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -23,7 +23,10 @@ class QuizQuestion extends Component {
                         
                     </ul>
                 </section>
-
+                {this.state.incorrectAnswer
+                    ? <p className="error">Sorry. that's not right</p>
+                    : null
+                }
             </main>
         );
     }
@@ -32,7 +35,16 @@ class QuizQuestion extends Component {
 
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
-        }        
+            this.setState({incorrectAnswer: false})
+        } else {
+            this.setState({incorrectAnswer: true})
+        }
+    }
+
+    constructor(props) {
+        super(props)
+
+        this.state = {incorrectAnswer: false};
     }
 }
 
